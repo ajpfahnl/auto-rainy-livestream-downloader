cd8e6deb5c81ec3fc93a7407f4d335e354c216fa
Arnold Pfahnl <ajpfahnl@gmail.com>
2021-09-24 13:04:12 -0700

    remove rgb option

--- filter.py
+++ filter.py
@@ -17,7 +17,7 @@ def mod_z(col: np.array, thresh: float=3.5) -> np.array:
     mod_z = mod_z[np.abs(mod_z) < thresh]
     return np.abs(mod_z)
 
-def is_rainy_video(cap: cv2.VideoCapture, seq_len: int, thresh: float=2, rgb: bool=False, show_figs: bool=False, bins: int=15) -> bool:
+def is_rainy_video(cap: cv2.VideoCapture, seq_len: int, thresh: float=2, show_figs: bool=False, bins: int=15) -> bool:
     '''
     NOTE: if seq_len is negative, all frames will be used
     '''
@@ -33,18 +33,11 @@ def is_rainy_video(cap: cv2.VideoCapture, seq_len: int, thresh: float=2, rgb: bo
 
     # extract intensities of middle pixel
     intensities = np.zeros(frame_count)
-    if rgb:
-        intensities_g = np.zeros_like(intensities)
-        intensities_r = np.zeros_like(intensities)
+
     for i in range(frame_count):
         res, frame = cap.read()
-        if not rgb:
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            intensities[i] = frame[w_mid, h_mid]
-        if rgb:
-            intensities[i] = frame[w_mid, h_mid, 0]
-            intensities_g[i] = frame[w_mid, h_mid, 1]
-            intensities_r[i] = frame[w_mid, h_mid, 2]
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        intensities[i] = frame[w_mid, h_mid]
 
     # assume gaussian distribution
     mu, sigma = np.mean(intensities), np.std(intensities)
@@ -58,19 +51,10 @@ def is_rainy_video(cap: cv2.VideoCapture, seq_len: int, thresh: float=2, rgb: bo
     rainy = True if percent_outliers >= thresh else False
 
     if show_figs:
-        if rgb:
-            plt_dims = (3, 1)
-            fig, axs = plt.subplots(*plt_dims)
-            axs[0].hist(intensities, bins=bins, color='b')
-            axs[1].hist(intensities_g, bins=bins, color='g')
-            axs[2].hist(intensities_r, bins=bins, color='r')
-            fig.tight_layout()
-            plt.show()
-        else:
-            plt.hist(intensities, bins=bins, color='b')
-            plt.plot(mu, 0, 'ro')
-            plt.plot([mu-sigma, mu+sigma], [0,0], 'o', color="orange")
-            plt.show()
+        plt.hist(intensities, bins=bins, color='b')
+        plt.plot(mu, 0, 'ro')
+        plt.plot([mu-sigma, mu+sigma], [0,0], 'o', color="orange")
+        plt.show()
 
     return rainy, mu, sigma, outliers, frame_count, percent_outliers
 
@@ -78,13 +62,12 @@ def main():
     parser = argparse.ArgumentParser(description='filters rainy and non-rainy videos')
     parser.add_argument('folder', help='folder with videos')
     parser.add_argument('-f', '--frames', type=int, default=-1, help='frames to process, defaults to all frames')
-    parser.add_argument('--rgb', default=False, action='store_true', help='process rgb channels separately')
     parser.add_argument('-t', '--threshold', type=float, default=2.0, help='threshold for the percentage of outliers to be considered rain drops, default is 2.0 (e.g. 2.0%%)')
     parser.add_argument('--plot', dest='plot_bool', default=False, action='store_true', help='displays plots of the histogram of intensities for each video')
     parser.add_argument('-b', '--bins', type=int, default=15, help='number of bins to display in the histogram plots')
     parser.add_argument('--csv', default=False, action='store_true', help='output csv format')
     args = parser.parse_args()
-    folder, plot_bool, frames, rgb, threshold, bins, csv_bool = args.folder, args.plot_bool, args.frames, args.rgb, args.threshold, args.bins, args.csv
+    folder, plot_bool, frames, threshold, bins, csv_bool = args.folder, args.plot_bool, args.frames, args.threshold, args.bins, args.csv
 
     if csv_bool:
         print('file,rainy,mean,std,outlier count,total intensities,percent outliers')
@@ -94,7 +77,7 @@ def main():
         cap = cv2.VideoCapture(os.path.join(folder, file))
         if (int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) == 0):
             continue
-        rainy, mu, sigma, outliers, total_intensities, percent_outliers = is_rainy_video(cap, frames, threshold, rgb, plot_bool, bins)
+        rainy, mu, sigma, outliers, total_intensities, percent_outliers = is_rainy_video(cap, frames, threshold, plot_bool, bins)
         if not csv_bool:
             print(f"\tnumber of outliers: {outliers}, total number of intensities: {total_intensities}, percent outliers: {percent_outliers:.2f}")
             print(f"\tmean: {mu:.2f}, std: {sigma:.2f}")
